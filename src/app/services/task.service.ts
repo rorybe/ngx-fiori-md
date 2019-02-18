c459d2d19be5f46c428287b6465177da6a23e262
Rory <rory@Air-II.local>
2019-02-19 00:53:20 +1000

    task header mock service and data

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,29 +1,54 @@
 import { Injectable } from '@angular/core';
-import {
-    AngularFirestore,
-    AngularFirestoreCollection
-} from 'angularfire2/firestore';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { BehaviorSubject, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import MockFirebase from 'mock-cloud-firestore';
+import { fixtureData, snapshot } from '../mocks/firestoretaskheader.mock';
+
 
 @Injectable()
 export class TaskService {
-    // tL$: AngularFirestoreCollection<any>;
     currentTask = new BehaviorSubject<any>(null);
 
     constructor(private db: AngularFirestore) {
-        // this.taskList$ = db.collection('taskheaders');
+
+        if (true !== true) {
+            const firebaseMock = new MockFirebase(fixtureData);
+            this.db = firebaseMock.firestore();
+            // tslint:disable-next-line:no-string-literal
+            this.db.collection('taskheaders')['__proto__'].snapshotChanges = () => {
+                const snaps = snapshot.map(snap => {
+                    // tslint:disable-next-line:no-string-literal
+                    snap.payload.doc['id'] = snap.payload.doc._document.proto.name.split('/')[6];
+                    // tslint:disable-next-line:no-string-literal
+                    snap.payload.doc['data'] = () => {
+                        const fields = snap.payload.doc._document.proto.fields;
+                        return {
+                            completionDeadLine: { seconds: new Date(fields.completionDeadLine.timestampValue).getTime() / 1000 },
+                            createdByName: fields.createdByName.stringValue,
+                            createdOn: { seconds: new Date(fields.createdOn.timestampValue).getTime() / 1000 },
+                            priority: fields.priority.stringValue,
+                            taskDefinitionName: fields.taskDefinitionName.stringValue,
+                            taskTitle: fields.taskTitle.stringValue,
+                            user: fields.user.stringValue
+                        };
+                    };
+                    return snap;
+                });
+                return of(snaps);
+            };
+        }
     }
 
     get taskList$() {
-        const taskList = this.db.collection('taskheaders')
+        return this.db.collection('taskheaders')
             .snapshotChanges()
             .pipe(map(actions => actions.map(a => {
                 const data = a.payload.doc.data();
                 const id = a.payload.doc.id;
                 return { ...data, id };
             })));
-        return taskList;
     }
 
     get selectedTask() {
