416b6721f90b7be716baac1766744f7a1f923c67
rorybe <rorber@outlook.com>
2019-02-19 11:27:46 +1000

    Added directives, removed equivalent pipes (#5)
    * converted colour class pipes to directives
    
    * fix pipe logic

---
+++ src/app/directives/taskduecolour.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, Input, ElementRef, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appTaskduecolour]'
+})
+export class TaskduecolourDirective implements AfterViewInit {
+
+  constructor(private el: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    debugger;
+    const dueStatus = this.el.nativeElement.innerHTML.trim();
+    const styles = this.el.nativeElement.style;
+    switch (dueStatus) {
+      case 'Overdue':
+        styles.color = 'red';
+        break;
+      case 'Expiring':
+        styles.color = 'orange';
+        break;
+      default:
+        break;
+    }
+  }
+
+}
---
+++ src/app/directives/taskprioritycolour.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, Input, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appTaskprioritycolour]'
+})
+export class TaskprioritycolourDirective implements AfterViewInit {
+
+  @Input('appTaskprioritycolour')
+  priority: string;
+
+  constructor(private el: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    const styles = this.el.nativeElement.style;
+    switch (this.priority) {
+      case 'High':
+        styles.color = 'red';
+        break;
+      case 'Medium':
+        styles.color = 'orange';
+        break;
+      case 'Low':
+        styles.color = 'green';
+        break;
+      default:
+        break;
+    }
+  }
+}
