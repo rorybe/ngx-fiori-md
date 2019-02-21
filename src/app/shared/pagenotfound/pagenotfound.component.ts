d5e70f42284f1e8cad11a19045cdbb90e4a0624f
Rory <rory@Air-II.local>
2019-02-21 23:40:50 +1000

    remove redundant import

--- src/app/shared/pagenotfound/pagenotfound.component.ts
+++ src/app/shared/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '../../core/services/translate.service';
 
 @Component({
