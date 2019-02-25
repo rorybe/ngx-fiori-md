49005b45fb2591534e2de6fb554830929ed924e0
rorybe <rorber@outlook.com>
2019-02-25 22:27:54 +1000

    setup compodoc, start commenting code (#20)

--- src/app/core/comments/comments.component.ts
+++ src/app/core/comments/comments.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { TranslateService } from '../services/translate.service';
 import { CommentService } from '../services/comment.service';
 
+/**
+ * Comments tab component in the Detail view
+ */
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss']
 })
-
 export class CommentsComponent {
 
   loading$ = this.commentService.loading$;
@@ -17,6 +19,9 @@ export class CommentsComponent {
     private translateService: TranslateService,
     private commentService: CommentService) { }
 
+  /**
+   * Returns translatable texts for current language (default English)
+   */
   get translatedTexts() {
     return this.translateService.i18n;
   }
