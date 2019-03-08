809e5ac4af87cb90424cf2c92ea37f90e41db853
rorybe <rorber@outlook.com>
2019-03-08 14:13:41 +1000

    test pipeline with test break (#27)

---
+++ src/app/testing/mockdata/taskList.mock.ts
@@ -0,0 +1,91 @@
+export const taskList = [{
+    completionDeadLine: {
+        seconds: 1551276000,
+        nanoseconds: 0
+    },
+    createdByName: 'Joe Root',
+    createdOn: {
+        seconds: 1549160503,
+        nanoseconds: 0
+    },
+    priority: 'High',
+    taskDefinitionName: 'Release Purchase Order',
+    taskTitle: 'Release Punter\'s Purchase Order ',
+    user: 'Ricky Ponting',
+    id: '9ioGbb5yBHa4CeKSUkuH'
+}, {
+    completionDeadLine: {
+        seconds: 1549321532,
+        nanoseconds: 0
+    },
+    createdByName: 'Brendon McCullum',
+    createdOn: {
+        seconds: 1548975932,
+        nanoseconds: 0
+    },
+    priority: 'Low',
+    taskDefinitionName: 'Approve Leave Request',
+    taskTitle: 'Approve Leave Request for Rahul',
+    user: 'Rahul Dravid',
+    id: 'YmFlwqPX2j22UK67MTel'
+}, {
+    completionDeadLine: {
+        seconds: 1550930400,
+        nanoseconds: 0
+    },
+    createdByName: 'Mark Nicholas',
+    createdOn: {
+        seconds: 1546488491,
+        nanoseconds: 0
+    },
+    priority: 'Medium',
+    taskDefinitionName: 'Approve Leave Request',
+    taskTitle: 'Lachlan\'s Leave Approval',
+    user: 'Lachlan Strongarm',
+    id: 'cUrsT10GRfjmLdOfomMq'
+}, {
+    completionDeadLine: {
+        seconds: 1546610400,
+        nanoseconds: 0
+    },
+    createdByName: 'Andrew Flintoff',
+    createdOn: {
+        seconds: 1543986120,
+        nanoseconds: 0
+    },
+    priority: 'Low',
+    taskDefinitionName: 'Approve Credit Limit',
+    taskTitle: 'Approve Credit Limit for Michael',
+    user: 'Michael Hussey',
+    id: 'gjImLKqDMuBy34WQrg9q'
+}, {
+    completionDeadLine: {
+        seconds: 1560866400,
+        nanoseconds: 0
+    },
+    createdByName: 'Chris Lynn',
+    createdOn: {
+        seconds: 1550152800,
+        nanoseconds: 0
+    },
+    priority: 'Medium',
+    taskDefinitionName: 'Sales Order Approval',
+    taskTitle: 'Approve Sales Order',
+    user: 'Rishabh Pant',
+    id: 'nFMCXSmvDtL9PC67iOAe'
+}, {
+    completionDeadLine: {
+        seconds: 1577714400,
+        nanoseconds: 0
+    },
+    createdByName: 'Nathan Lyon',
+    createdOn: {
+        seconds: 1542546907,
+        nanoseconds: 0
+    },
+    priority: 'High',
+    taskDefinitionName: 'Approve Aman\'s Overtime',
+    taskTitle: 'Overtime Approval',
+    user: 'Aman Sharma',
+    id: 'yJHX8Utr4QFygHNcDfOL'
+}];
