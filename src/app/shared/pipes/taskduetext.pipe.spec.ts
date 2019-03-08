08f38075019fc562c0b7c68ff3e174e82446c6ec
Rory <rory@Air-II.local>
2019-03-08 14:10:35 +1000

    test pipeline with test break

--- src/app/shared/pipes/taskduetext.pipe.spec.ts
+++ src/app/shared/pipes/taskduetext.pipe.spec.ts
@@ -1,8 +1,28 @@
 import { TaskDueTextPipe } from './taskduetext.pipe';
+import { TaskStatus } from '../../models/TaskStatus';
 
 describe('TaskDueTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TaskDueTextPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: TaskDueTextPipe;
+    const ONE_WEEK_IN_MS = 604800000;
+
+    beforeEach(() => {
+        pipe = new TaskDueTextPipe();
+    });
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+        expect(pipe).toBeFalsy();
+    });
+
+    it('should return Overdue if in the past', () => {
+        expect(pipe.transform(1, 2)).toBe(TaskStatus.overdue);
+    });
+
+    it('should return Expiring if in overdue in the next week', () => {
+        expect(pipe.transform(2, 1)).toBe(TaskStatus.expiring);
+    });
+
+    it('shouldn\t return a value if overdue in more than a week', () => {
+        expect(pipe.transform(ONE_WEEK_IN_MS + 2, 1)).toBeUndefined();
+    });
 });
