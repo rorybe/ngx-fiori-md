c83b45e3f241d6ea725e151e871f4b1713d47e69
rorybe <rorber@outlook.com>
2019-03-11 20:11:29 +1000

    Add tile component tests (#30)
    * added tile component tests
    
    * r

--- src/app/core/tile/tile.component.spec.ts
+++ src/app/core/tile/tile.component.spec.ts
@@ -1,28 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TileComponent } from './tile.component';
 import { TestingModule } from 'src/app/testing/testing.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
+const taskId = '1';
+const router = {
+    navigate: jasmine.createSpy('navigate')
+};
 
 describe('TileComponent', () => {
-  let component: TileComponent;
-  let fixture: ComponentFixture<TileComponent>;
+    let component: TileComponent;
+    let fixture: ComponentFixture<TileComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, TestingModule],
-      declarations: [TileComponent]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule, TestingModule],
+            declarations: [TileComponent],
+            providers: [
+                { provide: Router, useValue: router },
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TileComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should navigate to the notfound route when there\'s no task ID for a task', () => {
+        component.setSelectedTaskId(null);
+        expect(router.navigate).toHaveBeenCalledWith(['notfound']);
+    });
+
+    it('should navigate to the detail route when there\'s a taskID', () => {
+        component.setSelectedTaskId(taskId);
+        expect(router.navigate).toHaveBeenCalledWith(['detail', taskId]);
+    });
 });
