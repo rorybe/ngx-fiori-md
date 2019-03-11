0af0a2056d97d4f0f2c94f3b60f0aa971500b9c7
rorybe <rorber@outlook.com>
2019-03-11 19:27:25 +1000

    Add master component tests
    * initial unit test setup with mock components
    
    * prelim tests, incl test in pipeline
    
    * revert deploy process
    
    * rever deploy process
    
    * add master component tests

--- src/app/core/master/master.component.spec.ts
+++ src/app/core/master/master.component.spec.ts
@@ -1,27 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MasterComponent } from './master.component';
-import { TestingModule } from 'src/app/testing/testing.module';
-import { TranslateService } from '../services/translate.service';
+import { TestingModule } from '../../testing/testing.module';
+import { taskList } from '../../testing/mockdata/taskList.mock';
+import { Task } from 'src/app/models/Task.model';
 
 describe('MasterComponent', () => {
-  let component: MasterComponent;
-  let fixture: ComponentFixture<MasterComponent>;
+    let component: MasterComponent;
+    let fixture: ComponentFixture<MasterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [TestingModule],
-      declarations: [MasterComponent]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [TestingModule],
+            declarations: [MasterComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MasterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MasterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should update task list when search term is provided', () => {
+        component.taskList = taskList as any;
+        component.searchTerm = 'approve';
+        component.onSearchModelChange();
+        expect(component.taskListSearchResults.length)
+            .toBeLessThan(component.taskList.length);
+    });
+
+    it('should sort tasks descending by deadline date on first sort', () => {
+        component.taskList = taskList as any;
+        fixture.whenStable().then(() => {
+            component.onSort();
+            expect(component.sorted).toBeTruthy();
+            expect(component.taskList[0].completionDeadLine.seconds)
+                .toBeLessThan(component.taskList[1].completionDeadLine.seconds);
+            expect(component.taskList[1].completionDeadLine.seconds)
+                .toBeLessThan(component.taskList[2].completionDeadLine.seconds);
+            expect(component.taskList[2].completionDeadLine.seconds)
+                .toBeLessThan(component.taskList[3].completionDeadLine.seconds);
+        });
+    });
 });
