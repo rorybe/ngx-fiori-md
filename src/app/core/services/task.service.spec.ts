0c4d3f7a509638fcc80acbe1729e3af663782df3
Rory <rory@Air-II.local>
2019-03-11 21:47:15 +1000

    task service tests

--- src/app/core/services/task.service.spec.ts
+++ src/app/core/services/task.service.spec.ts
@@ -1,15 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TaskService } from './task.service';
-import { TestingModule } from 'src/app/testing/testing.module';
+import { TestingModule } from '../../testing/testing.module';
+import { taskList } from '../../testing/mockdata/taskList.mock';
+import { FirestoreMock } from 'src/app/testing/services/firestore.mock.service';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+const taskId = '1';
+const tabIndex = 1;
 
 describe('TaskService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [TestingModule]
-  }));
+    let service: TaskService;
+    let angularFirestore: AngularFirestore;
 
-  it('should be created', () => {
-    const service: TaskService = TestBed.get(TaskService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [TestingModule],
+            providers: [
+                { provide: AngularFirestore, useValue: FirestoreMock }
+            ]
+        });
+        service = TestBed.get(TaskService);
+        angularFirestore = TestBed.get(AngularFirestore);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should abort getting task data if no task ID is provided', () => {
+        const spy = jasmine.createSpyObj('AngularFirestore', ['doc']);
+        service.load(null, tabIndex);
+        expect(spy.doc).not.toHaveBeenCalled();
+    });
+
+    it('should update disabled tabs on load of a selected task', () => {
+        spyOn(angularFirestore, 'doc').and.callThrough();
+        service.load(taskId, null);
+        expect(angularFirestore.doc).toHaveBeenCalledWith(`taskheaders/${taskId}`);
+    });
+
+    it('shouldn\'t call another service if there\'s no tab ID', () => {
+
+    });
+
+    // When getTaskList$ is called with taskheaders mockdata, id, and data pass back
+    it('should ', () => {
+    });
 });
