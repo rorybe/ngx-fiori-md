1e78ada9064764a2b25af5d4af9463b8318ca334
Rory <rory@Air-II.local>
2019-03-24 15:38:36 +1000

    e2e test setup

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -6,11 +6,19 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
+    // @TODO GET RID OF THIS
+    browser.sleep(5000);
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-fiori-md!');
+  it('should display consistent task Created By name', (done) => {
+    const tile = page.getTile(2);
+    const masterCreatedBy = page.getTileTaskCreatedBy(tile);
+    masterCreatedBy.click().then(() => {
+      const detailCreatedBy = page.getTaskCreatedBy();
+      expect(masterCreatedBy.getText()).toBe(detailCreatedBy.getText());
+      done();
+    });
   });
 
   afterEach(async () => {
